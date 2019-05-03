ee9af6081e232715b7413542a7f1030c0b79f4db
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2019-05-03 00:05:53 -0300

    Login layout and animations done

--- Jeni/App Sections/Login/ViewController/LoginViewController.swift
+++ Jeni/App Sections/Login/ViewController/LoginViewController.swift
@@ -19,20 +19,54 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var swapOptionsLabel: UILabel!
     
     @IBOutlet weak var signButton: JeniButton!
+    @IBOutlet weak var swapButton: UIButton!
     
-    var isHidden: Bool = false
+    var isHidden: Bool = true
+    
+    let signInText = "Already a patient? Try "
+    let signUpText = "Are you a new patient? Try "
+    let signInTextButton = "Sign In"
+    let signUpTextButton = "Sign Up"
+    
+    enum SignOption {
+        case signIn
+        case signUp
+    }
+    
+    var signOption: SignOption!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        signOption = SignOption.signIn
     }
     
     @IBAction func signButtonAction(_ sender: Any) {
+        // TODO : Sign In
+        
+        // TODO : Sign Up
+    }
+    
+    @IBAction func swapSignOptionsAction(_ sender: Any) {
         _ = [UIView .animate(withDuration: 0.4,
                              delay: 0.1,
                              options: .curveEaseIn,
                              animations: {
-            self.confirmPasswordStackView.isHidden = !self.isHidden
-            self.isHidden = !self.isHidden
+                                switch self.signOption {
+                                case .signIn?:
+                                    self.swapOptionsLabel.text = self.signInText
+                                    self.swapButton.setTitle(self.signInTextButton, for: .normal)
+                                    self.signButton.setTitle(self.signUpTextButton, for: .normal)
+                                    self.confirmPasswordStackView.isHidden = !self.isHidden
+                                    self.signOption = SignOption.signUp
+                                case .signUp?:
+                                    self.swapOptionsLabel.text = self.signUpText
+                                    self.swapButton.setTitle(self.signUpTextButton, for: .normal)
+                                    self.signButton.setTitle(self.signInTextButton, for: .normal)
+                                    self.confirmPasswordStackView.isHidden = self.isHidden
+                                    self.signOption = SignOption.signIn
+                                case .none:
+                                    fatalError()
+                                }
         }, completion: nil)]
     }
 }
