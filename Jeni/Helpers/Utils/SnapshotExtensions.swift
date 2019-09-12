66cc1809bc2f32776b452b1f77d829622b9cae2a
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2019-09-12 17:06:46 -0300

    Update SnapshotExtensions.swift

--- Jeni/Helpers/Utils/SnapshotExtensions.swift
+++ Jeni/Helpers/Utils/SnapshotExtensions.swift
@@ -11,7 +11,6 @@ import FirebaseFirestore
 
 extension QueryDocumentSnapshot {
     func decoded<T: Decodable>() throws -> T {
-        print(data())
         let jsonData = try JSONSerialization.data(withJSONObject: data(), options: [])
         let object = try JSONDecoder().decode(T.self, from: jsonData)
         return object
